0e203d2c648bc71cfd8930309f521093a3157b8c
Henning Matyschok <henning.matyschok@outlook.com>
2021-05-11 18:22:57 +0200

    Cleanup tasks performed.

--- sys/dev/stge/if_stge.c
+++ sys/dev/stge/if_stge.c
@@ -30,7 +30,6 @@
  */
 /*
  * Copyright (c) 2018 Henning Matyschok
- * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
